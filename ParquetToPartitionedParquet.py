5d58645983d5fdaf2e3eca8b3816b28429c771f4
Tyler Wilson <twtylerwilson@gmail.com>
2022-10-25 22:03:28 -0600

    adjusted csv read in

--- ParquetToPartitionedParquet.py
+++ ParquetToPartitionedParquet.py
@@ -1,10 +1,33 @@
+from datetime import datetime
+
 import pyarrow as pa
-import pyarrow.parquet as pq
+# import pyarrow.parquet as pq
 import pyarrow.dataset as ds
+from pyarrow import csv
 
-allData = pq.read_table("E:/eBirdData/allData.parquet")
+print(datetime.now.time())
 
-ds.write_dataset(allData, "C:/Users/Meso/Documents/GitHub/BirdExplorer/data/countrySplits", format="parquet",
-                 partitioning=ds.partitioning(
-                    pa.schema([allData.schema.field("COUNTRY")])
-                ))
+csv_file = "E:/ebd_relAug-2022/ebd_relAug-2022.txt"
+parquet_folder = "E:/eBirdData/partCountry"
+
+inputData = csv.open_csv(
+    csv_file, 
+    parse_options = csv.ParseOptions(
+        delimiter = "\t"
+        ),
+    convert_options = csv.ConvertOptions(
+        strings_can_be_null = True,
+        column_types = {
+            'REASON' : pa.string()
+        }
+    ))
+
+print("moving to write dataset")
+print(datetime.now.time())
+
+ds.write_dataset(inputData, parquet_folder, format = "parquet", partitioning = ds.partitioning(
+    pa.schema([inputData.schema.field("COUNTRY")])
+))
+
+print("done")
+print(datetime.now.time())
--- eBirdTransformTxt.py
+++ eBirdTransformTxt.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import pyarrow.dataset as ds
 
-parquet_file = "E:/eBirdData/allData.parquet"
+parquet_folder = "E:/eBirdData/partCountry"
 csv_file = "E:/ebd_relAug-2022/ebd_relAug-2022.txt"
 
 chunkSize = 100_000
@@ -16,7 +17,12 @@ for i, chunk in enumerate(csv_stream):
     if i == 0:
         #  Guess the schema of the CSV file from the first chunk
         parquet_schema = pa.Table.from_pandas(df=chunk).schema
-        parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
+        chunkTable = pa.Table.from_pandas(chunk, schema=parquet_schema)
+        parquet_writer = ds.write_dataset(chunk, parquet_folder, format="parquet",
+                 partitioning=ds.partitioning(
+                    pa.schema([chunkTable.schema.field("COUNTRY")])
+                ))
+        # parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
         
     chunkTable = pa.Table.from_pandas(chunk, schema=parquet_schema)
      # write to new parquet file   
